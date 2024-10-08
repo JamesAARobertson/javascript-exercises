544d6e68fec9d5ba871bddcac029e61e981f4f57
James Robertson <jaarobertson92@gmail.com>
2024-10-08 16:08:15 +0100

    sumAll all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // initialise count to zero
+    let sum = 0
+    // identify the lowest and highest value
+    let min = Math.min(a, b)
+    let max = Math.max(a, b)
+
+    if (a < 0 || b < 0) return "ERROR" 
+
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"
+
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+    return sum
 
 };
 
