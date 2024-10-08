f27b091826c75f78203486a841669561aa512c07
James Robertson <jaarobertson92@gmail.com>
2024-10-08 13:06:27 +0100

    repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedStr="";
+    for (let i = 0; i < num; i++) {
+        repeatedStr += string;
+    }
+    return repeatedStr
 
 };
 
