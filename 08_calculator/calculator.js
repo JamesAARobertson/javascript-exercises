7552ae2dfa5b185dc555b3cbd708b9f451b32a29
James Robertson <jaarobertson92@gmail.com>
2024-10-09 13:15:22 +0100

    multiply and power all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,15 +6,17 @@ const subtract = function (a, b) {
   return a -b
 };
 
-const sum = function (arr) {
- return arr.reduce((total, current) => {
-  return total + current
- }, 0)
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function () {};
+const multiply = function (array) {
+  return array.reduce((total, current) => total * current)
+};
 
-const power = function () {};
+const power = function (a, b) {
+  return a ** b
+};
 
 const factorial = function () {};
 
