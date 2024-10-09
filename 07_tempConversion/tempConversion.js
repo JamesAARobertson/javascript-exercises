11b19ec3d29c11bfeb252ef45802e6aad364aa86
James Robertson <jaarobertson92@gmail.com>
2024-10-09 11:16:21 +0100

    tempconversion all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = Math.round((5/9) * (farenheit - 32) * 10) / 10
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit = Math.round(((celsius * (9/5)) + 32) * 10) / 10
+  return farenheit
 };
 
 // Do not edit below this line
