0e9b9fae9bcd7e5f5d9ac1f3524e5e586df2e01f
James Robertson <jaarobertson92@gmail.com>
2024-10-09 11:29:03 +0100

    minor reformatting

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,15 @@
-const convertToCelsius = function(farenheit) {
-  let celsius = Math.round((5/9) * (farenheit - 32) * 10) / 10
-  return celsius
+const convertToCelsius = function (farenheit) {
+  let celsius = Math.round((5 / 9) * (farenheit - 32) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function(celsius) {
-  let farenheit = Math.round(((celsius * (9/5)) + 32) * 10) / 10
-  return farenheit
+const convertToFahrenheit = function (celsius) {
+  let farenheit = Math.round((celsius * (9 / 5) + 32) * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
