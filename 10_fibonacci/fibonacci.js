a6589ec35b72a6f593d0cf716d2f67fb174767d1
James Robertson <jaarobertson92@gmail.com>
2024-10-09 16:33:32 +0100

    fibonacci sequence all tests passed. Tricky one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (nArg) {
+if (typeof nArg !== "number") {
+    n = parseInt(nArg)
+} else {
+    n = nArg
+}
 
+  // catch first fib number
+  if (n == 0) return 0;
+  // catch negative numbers
+  if (n < 0) return "OOPS";
+
+  //    initialise two variables to keep track of fibonacci numbers. This will also provide 1 for 2nd fib num
+  let firstPrev = 1;
+  let secondPrev = 0;
+
+  for (i = 2; i <= n; i++) {
+    // calculate current number
+    let current = firstPrev + secondPrev;
+    // update secondPrev to the last fib number
+    secondPrev = firstPrev;
+    // update firstPrev to current fib number
+    firstPrev = current;
+  }
+  return firstPrev;
 };
 
 // Do not edit below this line
