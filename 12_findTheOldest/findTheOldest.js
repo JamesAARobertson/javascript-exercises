93c8becba399d32fd87b766640d229aafcd5db2c
James Robertson <jaarobertson92@gmail.com>
2024-10-10 14:11:54 +0100

    findTheOldest all tests passed. Remember for reduce as it can be tricky!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   let oldestPerson = people.reduce((oldest, person) => {
+    let currentYear = new Date().getFullYear()
+        let age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        let oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
 
+        return age > oldestAge ? person : oldest
+    })
+return oldestPerson
 };
 
 // Do not edit below this line
